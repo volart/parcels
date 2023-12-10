fd9fe12d89467fac10bb945f513d6430da8522b5
volart <artem.volobuev@icloud.com>
2023-12-10 15:53:16 +0200

    update routes with parcels_list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { InsertParcelFormComponent } from './insert-parcel-form/insert-parcel-form.component';
+import { ParcelsListComponent } from './parcels-list/parcels-list.component';
 
 export const routes: Routes = [
   {
     path: 'insert_parcel',
     component: InsertParcelFormComponent,
     title: 'Insert Parcel Form',
+  },
+  {
+    path: 'parcels_list',
+    component: ParcelsListComponent,
+    title: 'Parcels List',
   },
 ];
