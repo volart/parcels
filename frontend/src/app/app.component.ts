3821d28d1122c0247f6a8da83b0f462fa2a89074
volart <artem.volobuev@icloud.com>
2023-12-10 12:48:07 +0200

    add routes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InsertParcelFormComponent } from './insert-parcel-form/insert-parcel-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'insert_parcel',
+    component: InsertParcelFormComponent,
+    title: 'Insert Parcel Form',
+  },
+];
