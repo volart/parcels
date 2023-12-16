31d6845e45642195bddbee2a9f85f4a45bece389
volart <artem.volobuev@icloud.com>
2023-12-16 15:57:02 +0200

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,19 @@ import express, { Request, Response, NextFunction } from "express";
 import "reflect-metadata";
 import parcelRoutes from "./api/parcel.routes";
 import parcelsRoutes from "./api/parcels.routes";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
+
+// CORS to allow requests from Angular frontend
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: 'POST,GET,PUT,OPTIONS,DELETE'
+}));
 
 app.use("/api/parcel", parcelRoutes);
 app.use("/api/parcels", parcelsRoutes);
